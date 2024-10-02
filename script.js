4e0c04cc36734b75538b2d45e34470ff1a59f170
ryancheung2 <ryancheung4050@gmail.com>
2024-10-02 19:05:41 +0800

    Coupon redeem and license update

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let gmailPrice = 10;
-let couponsTen = {
+let coupons = {
     "10Release": 10,
-    "GmailPro2024": 10
+    "GmailPro2024": 20,
+    "CISHZ": 30,
+    "S201A": 40
 };
 
-function redeemCoupon(couponsTen, gmailPrice) {
+function redeemCoupon(coupons, gmailPrice) {
     let inputCoupon = document.querySelector('input').value; 
     
-    if(couponsTen[inputCoupon]){
-        let discount = couponsTen[inputCoupon];
+    if(coupons[inputCoupon]){
+        let discount = coupons[inputCoupon];
         let discountedPrice = gmailPrice - (gmailPrice * (discount / 100));
         document.getElementById('gPrice').innerText = discountedPrice;
-        alert("Congratulations! You got a 10% discount!")
+        alert(`Congratulations! You got a ${coupons[inputCoupon]}% discount!`);
     } else {
         alert("Invalid Coupon");
     }
 }
 
 document.querySelector('input').addEventListener('change', function() {
-    redeemCoupon(couponsTen, gmailPrice);
+    redeemCoupon(coupons, gmailPrice);
 });
