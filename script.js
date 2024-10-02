0e21a314baef0ac87f3b6f43bcf3c5d2aba7f88d
ryancheung2 <ryancheung4050@gmail.com>
2024-10-02 12:20:02 +0800

    Redeem Coupon Function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let gmailPrice = 10;
+let couponsTen = {
+    "10Release": 10,
+    "GmailPro2024": 10
+};
 
+function redeemCoupon(couponsTen, gmailPrice) {
+    let inputCoupon = document.querySelector('input').value; 
+    
+    if(couponsTen[inputCoupon]){
+        let discount = couponsTen[inputCoupon];
+        let discountedPrice = gmailPrice - (gmailPrice * (discount / 100));
+        document.getElementById('gPrice').innerText = discountedPrice;
+        alert("Congratulations! You got a 10% discount!")
+    } else {
+        alert("Invalid Coupon");
+    }
+}
+
+document.querySelector('input').addEventListener('change', function() {
+    redeemCoupon(couponsTen, gmailPrice);
+});
