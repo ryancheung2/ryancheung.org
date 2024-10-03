272fbb3a07e1978a741b75128fe7b3a2ddfe8f3b
ryancheung2 <ryancheung4050@gmail.com>
2024-10-03 11:52:21 +0800

    Coupon encryption and autoupdated variables

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 let gmailPrice = 10;
-let coupons = {
-    "10Release": 10,
-    "GmailPro2024": 20,
-    "CISHZ": 30,
-    "S201A": 40
+
+let encodedCoupons = { //this is to prevent people from accessing the coupons from the console
+    [btoa("10Release")]: btoa(10),       
+    [btoa("GmailPro2024")]: btoa(20),
+    [btoa("CISHZ")]: btoa(30),
+    [btoa("S201A")]: btoa(40)
 };
 
-function redeemCoupon(coupons, gmailPrice) {
+let discountedPrice;
+
+function redeemCoupon(encodedCoupons, gmailPrice) {
     let inputCoupon = document.querySelector('input').value; 
+    let encodedInputCoupon = btoa(inputCoupon); 
     
-    if(coupons[inputCoupon]){
-        let discount = coupons[inputCoupon];
+    if (encodedCoupons[encodedInputCoupon]) {
+        let discount = atob(encodedCoupons[encodedInputCoupon]);
         let discountedPrice = gmailPrice - (gmailPrice * (discount / 100));
         document.getElementById('gPrice').innerText = discountedPrice;
-        alert(`Congratulations! You got a ${coupons[inputCoupon]}% discount!`);
+        alert(`Congratulations! You got a ${discount}% discount!`);
     } else {
         alert("Invalid Coupon");
     }
 }
 
 document.querySelector('input').addEventListener('change', function() {
-    redeemCoupon(coupons, gmailPrice);
+    redeemCoupon(encodedCoupons, gmailPrice);
 });
+
+setInterval(() => {
+    encodedCoupons = {
+        [btoa("10Release")]: btoa(10),
+        [btoa("GmailPro2024")]: btoa(20),
+        [btoa("CISHZ")]: btoa(30),
+        [btoa("S201A")]: btoa(40)
+    };
+    if (discountedPrice < 6) {
+        discountedPrice = 10;
+        document.getElementById('gPrice').innerText = discountedPrice;
+    }
+    gmailPrice = 10;
+}, 10); 
